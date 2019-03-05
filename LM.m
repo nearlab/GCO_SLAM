d93cfac027a0161fd52bb06f2107cbbc20558f02
coreymarcus <corey.marcus@gmail.com>
2019-03-05 16:39:43 -0600

    updated LM

--- LM.m
+++ LM.m
@@ -1,1 +1,13 @@
-% test test test
+function [x_hat] = LM(H, z, h, x0)
+% Levenberg-Marquart Nonlinear Optimization
+% Based on algorithm from Humphrey's Course Notes for Estimation
+% 
+% Our goal is to minimize J = ||z - h(x)||^2
+% INPUTS:
+% 
+% H - Jacobian of h with respect to x, evaluated at the current estimate...
+% Does this mean it shouldn't be an input????
+
+x_hat = h(x0);
+
+end
