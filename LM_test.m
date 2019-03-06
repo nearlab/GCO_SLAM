32f1e4416bc363d5e525700c19566a92ec55c140
coreymarcus <corey.marcus@gmail.com>
2019-03-06 12:39:50 -0600

    Finished LM Algo and wrote test script

---
+++ LM_test.m
@@ -0,0 +1,44 @@
+clear
+close all
+clc
+
+%max iterations
+maxIter = 200;
+
+%tolerance
+tol = 1E-6;
+
+%symbolic variables
+syms x1 x2 x3 'real'
+x = [x1 x2 x3]';
+
+%create function
+h = [sin(x1); cos(x2) + x3^2];
+
+%create truth
+x_true = [1 2 3]';
+
+%create measurement
+z = double(subs(h,x,x_true));
+
+%create weighting function
+Q = eye(length(z));
+
+%create initial guess
+x0 = x_true + .01*randn(3,1);
+
+%call function
+[x_hat, ii] = LM(z, h, x0, Q, tol, maxIter);
+
+disp('LM Performance')
+norm(x_hat - x_true)
+
+disp('Initial Performance')
+norm(x0 - x_true)
+
+%look at matlab default performance
+X = lsqnonlin(@(y)(z-[sin(y(1)); cos(y(2)) + y(3)^2]),x0);
+
+disp('Default Performance')
+norm(X - x_true)
+
