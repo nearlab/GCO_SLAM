63012643a114ff36701f90428950e569a4cc7bc6
coreymarcus <corey.marcus@gmail.com>
2019-03-05 18:11:08 -0600

    worked on LM algorithm

--- LM.m
+++ LM.m
@@ -1,13 +1,34 @@
-function [x_hat] = LM(H, z, h, x0)
+function [x_hat] = LM(z, h, x0, Q, tol, maxIter)
 % Levenberg-Marquart Nonlinear Optimization
 % Based on algorithm from Humphrey's Course Notes for Estimation
 % 
-% Our goal is to minimize J = ||z - h(x)||^2
+% Our goal is to minimize J = (z - h(x))'*Q*(z - h(x))
+% 
 % INPUTS:
 % 
-% H - Jacobian of h with respect to x, evaluated at the current estimate...
-% Does this mean it shouldn't be an input????
+% z - nx1 measurement vector
+% h - nonlinear function of x
+% x0 - initial guess for x
+% Q - weighting matrix 
+% tol - tolerance for convergence of J
+% maxIter - maximum number of iterations in case of operator error
+% 
+% OUTPUTS:
+% 
+% x_hat - estimated x values
 
-x_hat = h(x0);
+
+%Compute J
+J = (z - h(x0))'*Q*(z - h(x0));
+
+%initialize loop counter
+ii = 1;
+
+%begin loop
+while(J >= tol && ii <= maxIter)
+    
+end
+
+
 
 end
