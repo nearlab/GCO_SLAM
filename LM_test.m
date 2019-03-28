14a289718842e8b98158d9780f1272e3e1b22932
coreymarcus <corey.marcus@gmail.com>
2019-03-27 21:41:27 -0500

    Update LM_test.m

--- LM_test.m
+++ LM_test.m
@@ -1,6 +1,6 @@
 clear
 close all
-clc
+% clc
 
 %random number seed
 rng(1);
@@ -16,7 +16,7 @@ syms x1 x2 x3 'real'
 x = [x1 x2 x3]';
 
 %create function
-h = [sin(x1) + cos(x2); x1*x3^2; x3];
+h = [x1 + x2; 2*x3 - x1; x3];
 
 % %blow up the size of h
 % h = [h' h' h' h' h' h' h' h']';
@@ -24,26 +24,24 @@ h = [sin(x1) + cos(x2); x1*x3^2; x3];
 %create truth
 x_true = [1 2 3]';
 
-%create measurement
-z = double(subs(h,x,x_true))+.1*randn(length(h),1);
-
-%create weighting function
-Q1 = [1 2 3;0 2 4; 0 0 1];
+%create covariance function
+Q1 = 10*[1 5 10;0 1 1; 0 0 1];
 Q = Q1'*Q1;
+
+%create measurement
+z = double(subs(h,x,x_true))+ mvnrnd(zeros(3,1),inv(Q))';
 
 %create initial guess
 x0 = x_true + .1*randn(length(x_true),1);
 
 %call function
-[x_hat, ii, J_out, pChange] = LM(z, h, x0, Q, tol, maxIter);
+% [x_hat, ii, J_out, pChange] = LM(z, h, x0, Q, tol, maxIter);
 
 % disp('LM Performance')
 % norm(x_hat - x_true)
 % 
 % disp('Initial Performance')
 % norm(x0 - x_true)
-
-x_hat
 
 
 %look at matlab default performance
@@ -52,7 +50,7 @@ x_hat
 [V, D] = eig(Q);
 
 %create function
-fun = @(y) Q1'*(z-[sin(y(1)) + cos(y(2)); y(1)*y(3)^2; y(3)]);
+fun = @(y) Q1*(z-[y(1) + y(2); 2*y(3) - y(1); y(3)]);
 options = optimoptions('lsqnonlin','Display','iter','Algorithm','levenberg-marquardt',...
     'FunctionTolerance',1e-8);
 X = lsqnonlin(fun,x0,[],[],options);
@@ -60,7 +58,7 @@ X = lsqnonlin(fun,x0,[],[],options);
 X
 
 %solve again without weighting
-fun = @(y) z-[sin(y(1)) + cos(y(2)); y(1)*y(3)^2; y(3)];
+fun = @(y) z-[y(1) + y(2); 2*y(3) - y(1); y(3)];
 options = optimoptions('lsqnonlin','Display','iter','Algorithm','levenberg-marquardt',...
     'FunctionTolerance',1e-8);
 X = lsqnonlin(fun,x0,[],[],options);
@@ -69,4 +67,8 @@ X
 % 
 % disp('Default Performance')
 % norm(X - x_true)
+
+H = [1 1 0;-1 0 2; 0 0 1];
+
+x_ls = (H'*Q*H)^(-1)*H'*Q*z
 
